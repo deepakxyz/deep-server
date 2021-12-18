952f13e3a9d19fe550fcd4da723b48a61e4ec463
deepakxyz <deepakrajan.grea@gamil.com>
2021-12-18 19:49:43 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="bg-black flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Deep Server | Deepak Rajan @ 2022</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon-2.png" />
       </Head>
 
       <main className='text-white flex flex-col items-center justify-center'>
